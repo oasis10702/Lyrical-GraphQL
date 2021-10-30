5eb7f68787e303f46d9f4f1ede3bbb28dba7dd4f
oasis10702 <oasis10702@gmail.com>
2021-10-30 17:58:44 +0900

    Add render songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,10 +3,17 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return <li>{song.title}</li>;
+    });
+  }
+
   render() {
-    return <div>SongList</div>;
+    return <div>{this.renderSongs()}</div>;
   }
 }
+
 const query = gql`
   {
     songs {

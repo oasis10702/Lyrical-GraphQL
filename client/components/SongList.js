5f51e5b3193153a5ea3242eef37ce41a39da2038
oasis10702 <oasis10702@gmail.com>
2021-10-31 21:04:31 +0900

    Fix

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  // @TODO: Fix
   renderSongs() {
     return this.props.data.songs.map(song => {
       return <li>{song.title}</li>;

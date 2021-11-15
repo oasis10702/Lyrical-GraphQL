f550dcace75c22576bde89ea9c7770cbbb1b4851
oasis10702 <oasis10702@gmail.com>
2021-11-15 21:09:01 +0900

    Add song create form

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react';
 
+class SongCreate extends Component {
+  render() {
+    return (
+      <div>
+        <form></form>
+      </div>
+    );
+  }
+}
+
+export default SongCreate;

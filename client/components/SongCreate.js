654b4d827a4956a09575c1bcd808e087bfd9aa17
oasis10702 <oasis10702@gmail.com>
2021-11-18 21:43:29 +0900

    Add state

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { title: '' };
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
         <form>
           <label>Song Title</label>
-          <input />
+          <input onChange={event => this.setState({ title: event.target.value })} value={this.state.title} />
         </form>
       </div>
     );

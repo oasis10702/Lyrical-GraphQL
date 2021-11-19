58effcedb06440924426d2d03c4527321dca034e
oasis10702 <oasis10702@gmail.com>
2021-11-19 22:22:26 +0900

    Add submit handler

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -7,11 +7,15 @@ class SongCreate extends Component {
     this.state = { title: '' };
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title</label>
           <input onChange={event => this.setState({ title: event.target.value })} value={this.state.title} />
         </form>

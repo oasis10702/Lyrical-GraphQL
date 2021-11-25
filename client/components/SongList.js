31ad39464561981caf0429eb74f541c740c14154
oasis10702 <oasis10702@gmail.com>
2021-11-25 22:39:44 +0900

    Fix

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -18,7 +18,6 @@ class SongList extends Component {
     if (this.props.data.loading) {
       return <div>Loading...</div>;
     }
-
     return (
       <div>
         <ul className="collection">{this.renderSongs()}</ul>

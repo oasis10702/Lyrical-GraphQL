2f8da2547903869fd6a1385e81809f87ab7dafdf
oasis10702 <oasis10702@gmail.com>
2021-12-22 19:09:10 +0900

    Fix gql

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import query from '../queries/fetchSongs';

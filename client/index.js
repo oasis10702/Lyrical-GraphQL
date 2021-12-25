9c8d32cabe1342f5ba65aed3489dfcf53530146e
oasis10702 <oasis10702@gmail.com>
2021-12-26 02:11:08 +0900

    Fix

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
-import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import './style/style.css';
 
 import App from './components/App.js';
 import SongList from './components/SongList';

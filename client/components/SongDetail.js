ce2cc954906bf2a852c844e8398432acd2057fd5
oasis10702 <oasis10702@gmail.com>
2022-01-01 19:21:10 +0900

    Add jsx

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class SongDetail extends Component {
-  render() {}
+  render() {
+    return (
+      <div>
+        <h3>Song Detail</h3>
+      </div>
+    );
+  }
 }
 
 export default SongDetail;

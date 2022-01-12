4558384d83e7a3213e2a26585f3b79b4084fb554
oasis10702 <oasis10702@gmail.com>
2022-01-12 19:43:19 +0900

    Add navigate link

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {

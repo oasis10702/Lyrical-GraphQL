2bd78a4ab99c5e6b342d33c9e4393f130440aeb5
oasis10702 <oasis10702@gmail.com>
2022-01-14 18:30:10 +0900

    Add song list link

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -13,7 +13,7 @@ class SongList extends Component {
     return this.props.data.songs.map(({ title, id }) => {
       return (
         <li key={id} className="collection-item">
-          {title}
+          <Link to={`/songs/${id}`}>{title}</Link>
           <i className="material-icons" onClick={() => this.onSongDelete(id)}>
             delete
           </i>

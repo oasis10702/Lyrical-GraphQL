301b9d836466f9ad7837e7eb3e21204ddc995a14
oasis10702 <oasis10702@gmail.com>
2022-01-18 18:57:29 +0900

    Add label and input

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class LyricCreate extends Component {
   render() {
-    return {};
+    return (
+      <form>
+        <label>Add a Lyric</label>
+        <input />
+      </form>
+    );
   }
 }
 

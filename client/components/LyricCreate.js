be7329c21630e730fa004ee1b0e7cf61ee53c28e
oasis10702 <oasis10702@gmail.com>
2022-01-22 19:08:57 +0900

    Add on submit

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -7,9 +7,13 @@ class LyricCreate extends Component {
     this.state = { content: '' };
   }
 
+  onSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
         <input value={this.state.content} onChange={event => this.setState({ content: event.target.value })} />
       </form>

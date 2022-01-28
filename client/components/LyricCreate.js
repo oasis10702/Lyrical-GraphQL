d4dca6e1d8aecb7c36f0fd2bd1fc1bc5f7c23318
oasis10702 <oasis10702@gmail.com>
2022-01-28 19:17:28 +0900

    Add mutation to on submit

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import { graphql } from 'apollo-client';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "apollo-client";
 
 class LyricCreate extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { content: '' };
+    this.state = { content: "" };
   }
 
   onSubmit(event) {
     event.preventDefault();
-    // @TODO
+
+    this.props.mutation({
+      variables: {
+        content: this.state.content,
+        // songId:
+      },
+    });
   }
 
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <label>Add a Lyric</label>
-        <input value={this.state.content} onChange={event => this.setState({ content: event.target.value })} />
+        <input
+          value={this.state.content}
+          onChange={(event) => this.setState({ content: event.target.value })}
+        />
       </form>
     );
   }

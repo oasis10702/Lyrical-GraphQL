17a674f18947c8400f4b2d3c5584d3a3604f0f85
oasis10702 <oasis10702@gmail.com>
2022-02-05 00:15:15 +0900

    Import

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { graphql } from "react-apollo";
 import { Link } from "react-router";
 import fetchSong from "../queries/fetchSong";
 import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 
 class SongDetail extends Component {
   render() {

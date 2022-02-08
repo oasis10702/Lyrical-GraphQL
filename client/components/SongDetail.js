506fff0dc19067f21aafd7547e3f5bf9e63be10e
oasis10702 <oasis10702@gmail.com>
2022-02-08 20:38:35 +0900

    Add lyric props

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -17,6 +17,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
         <LyricCreate songId={this.props.params.id} />
       </div>
     );
